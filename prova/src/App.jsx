80a985ff6527c8108157f7cbbf8b9529de0dfd88
dariojessula <115502273+dariojessula@users.noreply.github.com>
2024-04-24 18:51:13 -0300

    Update App.jsx

--- prova/src/App.jsx
+++ prova/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Inicio from './Inicio'; // Renomeado de HomePage
-import Asteroid from './Asteroid'; // Renomeado de AsteroidPage
-import Sobre from './Sobre'; // Renomeado de AboutPage
-import Navbar from './Navbar'; // Certifique-se de que este arquivo também esteja atualizado para usar os novos caminhos
-import './App.css'; // Certifique-se de que o CSS está correto e carregado
+import Inicio from './Inicio'; 
+import Asteroid from './Asteroid'; 
+import Sobre from './Sobre'; 
+import Navbar from './Navbar'; 
+import './App.css'; 
 
 // criar rotas entre as paginas 
 
